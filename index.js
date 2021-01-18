01b0220d7e522bff1dffde33c56e319f0f2c1597
socratone <gim2origin@gmail.com>
2021-01-18 18:08:57 +0900

    index.js를 root 위치로 이동
    express가 root를 못찾는 이슈가 있다.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const routes = require('./routes');
+const routes = require('./src/routes');
 
 app.use(cors());
 
